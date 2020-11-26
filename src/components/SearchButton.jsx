1ba77e2425464a488e90ab6692a1b1525b8a7eed
unknown <email@example.com>
2020-11-26 09:20:59 +0100

    button update

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import Button from "@material-ui/core/Button";
 import { fetchData } from "../redux/actions/dataActions";
 import { fetchQuery } from "../redux/actions/queryActions";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const SearchButton = ({ setQuery, setError, query, setValue, setJoke, value }) => {
   const dispatch = useDispatch();
@@ -26,9 +27,10 @@ const SearchButton = ({ setQuery, setError, query, setValue, setJoke, value }) =
 
   return (
     <div className="inControll">
+      <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
       <Button
 
-        className="button"
+      className="button"
         size="large"
         variant="contained"
         color="primary"
@@ -37,6 +39,22 @@ const SearchButton = ({ setQuery, setError, query, setValue, setJoke, value }) =
       >
         {query ? "Random Joke by query" : (value ? "Random Joke by category" : "Random Joke")}
       </Button>
+      {query ? <Button
+
+      className="button"
+        size="large"
+        variant="contained"
+        color="secondary"
+        style={{ width: "80px", fontSize: "20px" }}
+        onClick={ () => setQuery("") }
+      >
+        clear
+      </Button>
+      
+      
+      : ""}
+     
+      </ButtonGroup>
     </div>
   );
 };
